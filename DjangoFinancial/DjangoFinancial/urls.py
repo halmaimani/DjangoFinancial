a8f013333aba8acd9066636e9abdc6c45c5a8da5
halmaimani <halmaimani@unomaha.edu>
2018-02-26 22:47:30 -0600

    My Django Financial

--- DjangoFinancial/DjangoFinancial/urls.py
+++ DjangoFinancial/DjangoFinancial/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 
 from django.urls import include, path
 from django.contrib import admin
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('financial/', include('financial.urls')),
