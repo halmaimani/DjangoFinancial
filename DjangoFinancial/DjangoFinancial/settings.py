40e134f8c4b12c69e130ab04efabd1f5b277e054
halmaimani <halmaimani@unomaha.edu>
2018-02-26 20:15:00 -0600

    Changed the HTML for the site.

--- DjangoFinancial/DjangoFinancial/settings.py
+++ DjangoFinancial/DjangoFinancial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fyyxr8y1lw7uv)n=l6pu3-^6gwhxfgc2kt8y=z7s#3ktp-w8j!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
